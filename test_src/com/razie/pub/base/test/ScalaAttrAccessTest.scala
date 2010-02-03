a43c135d4750ea5ded8572d4747158c5f123807a
razie <razie@razie.com>
2010-02-03 13:46:47 +0000

    moved AA to razie

---
+++ test_src/com/razie/pub/base/test/ScalaAttrAccessTest.scala
@@ -0,0 +1,21 @@
+/**
+ * Razvan's public code. Copyright 2008 based on Apache license (share alike) see LICENSE.txt for
+ * details.
+ */
+package com.razie.pub.base.test
+
+import org.scalatest.junit._
+import com.razie.pub.base._
+import org.scalatest.SuperSuite
+
+/**
+ * testing the assets
+ * 
+ * @author razvanc99
+ */
+class ScalaAttrAccessTest extends JUnit3Suite {
+    val aa = new AttrAccessImpl("attr1:string=val1,attr2:int=2");
+
+   def testString() = expect ("2") { JavaAttrAccessImpl.fromString(aa.toString).a("attr2") }
+
+}
