68d6d658d573d2cfb3f906a98d62e23169f96a9c
Razie <razie@razie.com>
2011-08-16 11:53:34 -0400

    moved the dsl collector in base

---
+++ base/src/test/scala/razie/dsl/test/DslCollectorTest.scala
@@ -0,0 +1,22 @@
+package razie.dsl.test
+
+import org.scalatest.junit._
+import razie.dsl._
+
+/** simple collector example */
+class DslCollectorTest extends JUnit3Suite {
+
+  case class A(i: Int) extends DslCollectable
+
+  def test1 = expect(A(1) :: A(2) :: Nil) {
+    val collected = new collection.mutable.ListBuffer[Any]()
+
+    DslCollector.collect { collected += _ }(1) { // start a collector level
+      A(1) //collects itself
+      A(2) //collects itself
+    } // collector ends
+
+    collected
+  }
+}
+
