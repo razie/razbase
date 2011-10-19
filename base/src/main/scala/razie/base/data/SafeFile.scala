0d4b786e65eb74ab5ac8b0e7a48bc43a0904d766
Razie <razie@razie.com>
2011-10-19 14:14:37 -0400

    use sync() on top of flush()

--- base/src/main/scala/razie/base/data/SafeFile.scala
+++ base/src/main/scala/razie/base/data/SafeFile.scala
@@ -4,20 +4,26 @@ import java.io.File
 import java.io.FileReader
 import scala.collection.mutable.ListBuffer
 import razie.Logging
+import java.io.FileOutputStream
+import java.io.FileInputStream
 
 /** safe log file - use it to safely store some records.
+ * 
+ * NOTE that this in fact is just a playground and this is nowhere near safe... it's just a bunch of tests...
  *
  *  will spawn a background thread that will read records from the file, to make sure they're there... unless the OS is
  *  messing with us
  *
  *  Note that it will open/create automatically, when object is created
  */
-class SafeFile[T](val path: String) extends Logging {
+class SafeFile[T] (val path: String) extends Logging {
   debug ("creating new file: " + path)
   val f = new File(path)
   f.createNewFile()
 
-  val fout = new FileWriter(f)
+  val MAX_TXN=1000
+  
+  val fout = new FileOutputStream(f)
   val fin = new FileReader(f)
   var closing = false // close() was called - waiting for reader to die
 
@@ -27,10 +33,19 @@ class SafeFile[T](val path: String) extends Logging {
 
   /** write a record - the continuation is called when the record's presence has been verified... */
   def writeAsync(buf: T)(continuation: => Unit) {
+    // 1. TODO backoff to be exponential
+    var shouldWait=true
+    while (shouldWait) {
+      synchronized {shouldWait = waiting.size >= MAX_TXN}
+      if (shouldWait) Thread.sleep (100)
+    }
+    
     synchronized {
       val wtf = (buf, () => continuation)
       waiting += wtf
-      fout.write(buf.toString + "\n") // yes, inside the synchronized... test from 1000 threads to see why :)
+      fout.write((buf.toString + "\n").getBytes()) // yes, inside the synchronized... test from 1000 threads to see why :)
+      fout.flush()
+      fout.getFD().sync
     }
   }
 
@@ -54,7 +69,7 @@ class SafeFile[T](val path: String) extends Logging {
           }
           else acc += c.toChar
         } else
-          Thread.sleep(100)
+          Thread.sleep(100) 
       } finally {
         synchronized {
           isClosing = closing && waiting.isEmpty
@@ -79,10 +94,10 @@ class SafeFile[T](val path: String) extends Logging {
 
 object SafeFileApp extends App {
   val sf = new SafeFile[String]("safe-" + System.currentTimeMillis().toString + ".txt")
-  val PREFIX="1234567890123456789012345678901234567890"
+  val PREFIX="1234567890123456789012345678901234567890-"
   var inc = 0
   val LOOPS = 1000
-  val THREADS = 1000
+  val THREADS = 100
 
   val start=System.currentTimeMillis()
   
@@ -100,5 +115,5 @@ object SafeFileApp extends App {
   
   val end=System.currentTimeMillis()
   
-  println ("took %d msec for a performance of %f records/sec".format(end-start, (THREADS*LOOPS*1000.0)/(end-start)))
+  println ("took %d msec for a performance of %.2f records/sec".format(end-start, (THREADS*LOOPS*1000.0)/(end-start)))
 }
